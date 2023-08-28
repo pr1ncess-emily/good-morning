6152f178b6db4114dbbee230c9468e6a9703032c
pr1ncess-emily <b3nb0t5000@gmail.com>
2023-08-28 05:44:58 -0600

    Bot now sends GIF in general channel on startup

--- main.py
+++ main.py
@@ -18,8 +18,24 @@ def close_gif_file(file):
 def create_discord_file(file):
     return discord.File(file, description="Two anime girls kissing with the caption \"Good Morning\"")
 
+def is_text_channel(channel):
+    return type(channel) is discord.TextChannel
+
+def get_general_channel():
+    for channel in client.get_all_channels():
+        if channel.name == 'general' and is_text_channel(channel):
+            return channel
+
+async def send_good_morning_gif():
+    gif = open_gif_file()
+    discord_file = create_discord_file(gif)
+    general = get_general_channel()
+    if general:
+        await general.send(file=discord_file)
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
+    await send_good_morning_gif()
 
 client.run(discord_token)
